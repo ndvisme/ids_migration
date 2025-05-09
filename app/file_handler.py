746b3857d62cd16a7595e008f06e14234fed6db1
nadav-sgh <nspier@sightd.io>
2025-05-09 20:51:21 +0300

    Add file type validation.

--- app/file_handler.py
+++ app/file_handler.py
@@ -2,6 +2,12 @@ import csv
 
 
 class FileHandler:
+
+    def extract_data(self, path: str):
+        if not path.endswith('.csv'):
+            raise ValueError('Illegal input file type')
+        pass
+
     def get_file(self, path: str):
         with open(path, mode='r') as file:
             reader = csv.reader(file)
