a871e537b238a1330a145723b71df364d04661fc
nadav-sgh <nspier@sightd.io>
2025-05-10 10:22:44 +0300

    Add extract data implementation.

--- app/file_handler.py
+++ app/file_handler.py
@@ -3,13 +3,28 @@ import csv
 
 class FileHandler:
 
-    def extract_data(self, path: str):
+    def validate_file_type(self, path: str):
         if not path.endswith('.csv'):
             raise ValueError('Illegal input file type')
-        pass
 
     def get_file(self, path: str):
+        self.validate_file_type(path)
+
         with open(path, mode='r') as file:
             reader = csv.reader(file)
             data = [row for row in reader]
+
         return data
+
+    def extract_data(self, path: str) -> list['OldToNewId']:
+        raw_data = self.get_file(path)
+
+        extracted_data = [OldToNewId(old=row[0], new=row[1]) for row in raw_data[1:] if len(row) >= 2]
+
+        return extracted_data
+
+
+class OldToNewId:
+    def __init__(self, old: str, new: str):
+        self.old = old
+        self.new = new
